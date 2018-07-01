fea489628e4c00e262ad6ea2fb0f7cb41a099aa6
cpyou <neptunesaber@gmail.com>
2018-07-01 21:33:24 +0800

    修改冒泡排序

--- python/sort.py
+++ python/sort.py
@@ -1,10 +1,13 @@
 a = [1, 5, 2, 11, 3, 24, 5, 5, 7, 3, 2, 6, 10, 0]
 print a
-for i in range(len(a) -1):
-    for j in range(i + 1, len(a)):
-        if (a[i] > a[j]):
-            tmp = a[i]
-            a[i] = a[j]
-            a[j] = tmp
+# 冒泡排序
+for i in range(len(a) - 1):
+    for j in range(len(a) -i -1):
+        if a[j ] > a[j +1]:
+            tmp = a[j]
+            a[j] = a[j + 1]
+            a[j +1] = tmp
+   # if endsort == 0:
+   #     break
     print a
 
