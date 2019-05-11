1f907ecf261f5460123fcc5117cb927fbd743801
cpyou <neptunesaber@gmail.com>
2019-05-11 17:42:35 +0800

    [ADD] 添加快速排序排

--- python/sort.py
+++ python/sort.py
@@ -1,13 +1,33 @@
 a = [1, 5, 2, 11, 3, 24, 5, 5, 7, 3, 2, 6, 10, 0]
-print a
+print(a)
 # 冒泡排序
-for i in range(len(a) - 1):
-    for j in range(len(a) -i -1):
-        if a[j ] > a[j +1]:
-            tmp = a[j]
-            a[j] = a[j + 1]
-            a[j +1] = tmp
-   # if endsort == 0:
-   #     break
-    print a
+def bubble_sort(data):
+    for i in range(len(a) - 1):
+        for j in range(len(a) -i -1):
+            if a[j ] > a[j +1]:
+                tmp = a[j]
+                a[j] = a[j + 1]
+                a[j +1] = tmp
+    print(data)
+    return data
 
+
+def quick_sort(data):
+    if len(data) >= 2:
+        mid = data[len(data)//2]
+        left, right = [], []
+        data.remove(mid)
+        for num in data:
+            if num >= mid:
+                right.append(num)
+            else:
+                left.append(num)
+        result = quick_sort(left) + [mid] + quick_sort(right)
+        print(result)
+        return result
+    else:
+        return data
+
+
+# bubble_sort(a)
+quick_sort(a)
