bef4847b6bd7a3ea84cef3ab2578d538e0100c51
cpyou <745690939@qq.com>
2023-11-08 17:23:03 +0800

    添加环清理二进制文件脚本

---
+++ shell/clear_binaries.sh
@@ -0,0 +1,10 @@
+# 循环清理二进制文件
+root_dir='/release'
+for line in `ls -d $root_dir`
+do
+    for service_name in `ls $line`
+    do
+        ls -d $root_dir/$service_name/* | grep $service_name- | tail -n +12
+        ls -d $root_dir/$service_name/* | grep $service_name- | tail -n +12 | xargs -d '\n' -r rm --
+    done
+done
